21f172522159c26c037dfcd9f2654b50332e8ec0
Thomas Mathews <thomas.c.mathews@gmail.com>
2021-07-08 13:27:01 -0700

    Implemented basic methods needed for our usecases.

--- klaviyo.go
+++ klaviyo.go
@@ -1,8 +1,19 @@
 // Klaviyo uses Person & Person interchangeably through their API documentation, we will use just Person
+// https://apidocs.klaviyo.com/reference/api-overview
 
 package klaviyo
 
-import "errors"
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
 
 type Consent string
 type Reason string
@@ -14,32 +25,134 @@ const (
 	ConsentDirect Consent = "directmail"
 	ConsentMobile Consent = "mobile"
 
-	ReasonBounced      Reason = "Bounced"
-	ReasonUnsubscribed Reason = "Unsubscribed"
-
+	// They have multiple endpoints unfortunately.
+	Endpoint   = "https://a.klaviyo.com/api"
 	EndpointV1 = "https://a.klaviyo.com/api/v1"
 	EndpointV2 = "https://a.klaviyo.com/api/v2"
 )
 
 var (
-	ErrNil = errors.New("not implemented")
+	ErrNoProfileIdentifier = errors.New("there is no unique profile identifier, must have email or phone number")
+	ErrBadResponse         = errors.New("bad API response")
 )
 
+type APIError struct {
+	Message string `json:"message"`
+}
+
+func (e *APIError) Error() string {
+	return e.Message
+}
+
 type Object struct {
-	Id           string `json:"id"`
-	Object       string `json:"object"` // person,
+	Id     string `json:"id"`
+	Object string `json:"object"` // e.g. person, $list
 }
 
 type Client struct {
-	APIKey string
+	// Sometimes called "token"
+	PublicKey string
+
+	// Sometimes seen as "api_key"
+	PrivateKey string
+
+	// The amount of time an HTTP API call should run for before it times out.
+	DefaultTimeout time.Duration
 }
 
+func (c *Client) req(method string, url *url.URL, out interface{}) error {
+	url.Query().Add("api_key", c.PrivateKey)
+	req, err := http.NewRequest(method, url.String(), nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Accept", "application/json")
+	client := http.Client{Timeout: c.DefaultTimeout}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	// All of Klaviyo's calls should return 200 otherwise it's an error.
+	// See more here: https://apidocs.klaviyo.com/reference/api-overview#errors
+	if res.StatusCode != http.StatusOK {
+		var err APIError
+		if err := json.NewDecoder(res.Body).Decode(&err); err != nil {
+			return ErrBadResponse
+		}
+		return &err
+	}
+	if out != nil {
+		return json.NewDecoder(res.Body).Decode(out)
+	}
+	return nil
+}
+
+// https://apidocs.klaviyo.com/reference/track-identify#identify
+// GET https://a.klaviyo.com/api/identify
+func (c *Client) Identify(person *Person) error {
+	if !person.HasProfileIdentifier() {
+		return ErrNoProfileIdentifier
+	}
+
+	payload := struct {
+		Token      string      `json:"token"`
+		Properties interface{} `json:"properties"`
+	}{
+		Token:      c.PublicKey,
+		Properties: person.GetMap(),
+	}
+	buf := bytes.NewBuffer([]byte{})
+	if err := json.NewEncoder(buf).Encode(&payload); err != nil {
+		return err
+	}
+	u := newEndpoint(Endpoint, "identify")
+	u.Query().Add("data", base64.URLEncoding.EncodeToString(buf.Bytes()))
+	return c.req(http.MethodGet, u, nil)
+}
+
+// https://apidocs.klaviyo.com/reference/profiles#get-profile
+// GET https://a.klaviyo.com/api/v1/person/person_id
 func (c *Client) GetPerson(personId string) (*Person, error) {
-	// GET v1/person/%s?api_key=%s
-	return nil, ErrNil
+	var p *Person
+	err := c.req(http.MethodGet, newEndpoint(EndpointV1, fmt.Sprintf("person/%s", personId)), p)
+	return p, err
 }
 
+// https://apidocs.klaviyo.com/reference/profiles#update-profile
+// PUT https://a.klaviyo.com/api/v1/person/person_id
+// Only works to update a persons attributes after they have been identified.
 func (c *Client) UpdatePerson(person *Person) error {
-	// PUT v1/person/%s?api_key=%s
-	return ErrNil
+	u := newEndpoint(EndpointV1, fmt.Sprintf("person/%s", person.Id))
+	for k, v := range person.GetMap() {
+		u.Query().Add(k, fmt.Sprintf("%v", v))
+	}
+	return c.req(http.MethodPut, u, person)
+}
+
+type ListPerson struct {
+	Id          string `json:"id"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phone_number"`
+	Created     string `json:"created"`
+}
+
+// https://apidocs.klaviyo.com/reference/lists-segments#list-membership
+// GET https://a.klaviyo.com/api/v2/list/list_id/members
+func (c *Client) InList(listId string, emails []string, phoneNumbers []string, pushTokens []string) ([]ListPerson, error) {
+	u := newEndpoint(EndpointV2, fmt.Sprintf("list/%s/members", listId))
+	u.Query().Add("emails", strings.Join(emails, ","))
+	u.Query().Add("phone_numbers", strings.Join(phoneNumbers, ","))
+	u.Query().Add("push_tokens", strings.Join(pushTokens, ","))
+	var res []ListPerson
+	err := c.req(http.MethodGet, u, &res)
+	return res, err
+}
+
+func newEndpoint(endpoint, path string) *url.URL {
+	u, err := url.Parse(endpoint)
+	if err != nil {
+		panic(err) // This should always work because endpoint should be typed correctly in this SDK!
+	}
+	u.Path = path
+	return u
 }
--- profile.go
+++ profile.go
@@ -1,36 +1,111 @@
 package klaviyo
 
-import "errors"
+import (
+	"encoding/json"
+	"reflect"
+	"strings"
+)
 
 type Person struct {
 	Object
 
 	// Below are special fields used by Klaviyo, they will be render in special UI for fancy-ness
 	// they are identified by the $ prefix in their JSON.
-	City         string `json:"$city"`
+	City         string   `json:"$city"`
 	Consent      []string `json:"$consent"`
-	Country      string `json:"$country"`
-	Email        string `json:"$email"`
-	FirstName    string `json:"$first_name"`
-	Image        string `json:"$image"`
-	LastName     string `json:"$last_name"`
-	Organization string `json:"$organization"`
-	PhoneNumber  string `json:"$phone_number"`
-	Region       string `json:"$region"`
-	Timezone     string `json:"$timezone"`
-	Title        string `json:"$title"`
-	Zip          string `json:"$zip"`
+	Country      string   `json:"$country"`
+	Email        string   `json:"$email"`
+	FirstName    string   `json:"$first_name"`
+	Image        string   `json:"$image"`
+	LastName     string   `json:"$last_name"`
+	Organization string   `json:"$organization"`
+	PhoneNumber  string   `json:"$phone_number"`
+	Region       string   `json:"$region"`
+	Timezone     string   `json:"$timezone"`
+	Title        string   `json:"$title"`
+	Zip          string   `json:"$zip"`
 
 	// Use these to have custom attributes tied to a user that can be used to create segments for lists.
-	Attributes   map[string]interface{}
+	Attributes map[string]interface{}
+}
+
+// A profile identifier is an email or phone number. In the case of SMS they must have a phone number.
+func (p *Person) HasProfileIdentifier() bool {
+	return !(strings.TrimSpace(p.Email) == "" && strings.TrimSpace(p.PhoneNumber) == "")
+}
+
+func (p *Person) GetMap() map[string]interface{} {
+	m := map[string]interface{}{}
+	for k, v := range p.Attributes {
+		m[k] = v
+	}
+	for k, v := range structToMap(p) {
+		m[k] = v
+	}
+	return m
 }
 
 func (p *Person) MarshalJSON() ([]byte, error) {
-	// TODO convert Person to a map[string]interface{} that overrides the attributes map
-	return nil, errors.New("not implemented")
+	return json.Marshal(p.GetMap())
 }
 
 func (p *Person) UnmarshalJSON(data []byte) error {
-	// TODO unmarshal data into profile and all others go into attributes
-	return errors.New("not implemented")
+	m := map[string]interface{}{}
+	if err := json.Unmarshal(data, m); err != nil {
+		return err
+	}
+	if err := json.Unmarshal(data, p); err != nil {
+		return err
+	}
+
+	// Remove the existing keys because they are part of the struct
+	cleanKeys := []string{
+		"id",
+		"object",
+		"$city",
+		"$consent",
+		"$country",
+		"$email",
+		"$first_name",
+		"$image",
+		"$last_name",
+		"$organization",
+		"$phone_number",
+		"$region",
+		"$timezone",
+		"$title",
+		"$zip",
+	}
+	for _, k := range cleanKeys {
+		delete(m, k)
+	}
+
+	p.Attributes = m
+	return nil
+}
+
+func structToMap(item interface{}) map[string]interface{} {
+	res := map[string]interface{}{}
+	if item == nil {
+		return res
+	}
+	v := reflect.TypeOf(item)
+	reflectValue := reflect.ValueOf(item)
+	reflectValue = reflect.Indirect(reflectValue)
+
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	for i := 0; i < v.NumField(); i++ {
+		tag := v.Field(i).Tag.Get("json")
+		field := reflectValue.Field(i).Interface()
+		if tag != "" && tag != "-" {
+			if v.Field(i).Type.Kind() == reflect.Struct {
+				res[tag] = structToMap(field)
+			} else {
+				res[tag] = field
+			}
+		}
+	}
+	return res
 }
